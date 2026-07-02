0011da0e6e32430964f46113bcc65ad1489d4da1
Samra <samrariaz514@gmail.com>
2026-07-02 21:46:41 +0500

    Fix flake8 formatting issues

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-# def add(a, b):
-#     return a + b
-
-# if __name__ == "__main__":
-#     print(add(2, 3))
-
 def add(a, b):
     return a + b
+
 
 def library_name():
     return "Library Management System"
 
+
 if __name__ == "__main__":
     print(library_name())
+
+
+# def add(a, b):
+#   return a + b
+# if __name__ == "__main__":
+#  print(add(2, 3))
+

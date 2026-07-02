d3eed7b84d07253b857b98c36974a74e68cd1f0e
Samra <samrariaz514@gmail.com>
2026-07-02 21:10:19 +0500

    Add application files and CI workflow

---
+++ app.py
@@ -0,0 +1,14 @@
+# def add(a, b):
+#     return a + b
+
+# if __name__ == "__main__":
+#     print(add(2, 3))
+
+def add(a, b):
+    return a + b
+
+def library_name():
+    return "Library Management System"
+
+if __name__ == "__main__":
+    print(library_name())
